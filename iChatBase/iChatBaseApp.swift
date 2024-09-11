253dadc333dd0aab633d5b818de90c205bcedf7e
Kaic Cesar <kaiccesar@MacBook-Pro-de-Kaic.local>
2024-09-11 05:25:22 -0300

    first commit

---
+++ iChatBase/iChatBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatBaseApp.swift
+//  iChatBase
+//
+//  Created by Kaic Cesar on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct iChatBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
